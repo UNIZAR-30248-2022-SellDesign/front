74c0a5480eb68f727c77050726a8f96ea713dba7
rubenDTD <736650@unizar.es>
2022-10-12 21:12:39 +0200

    Placeholder pagina principal

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+
+
+  products: Product[] = [
+    {productName:"Sudadera",designName:"buah",price:10,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:20,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:30,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:40,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:50,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:60,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:70,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"},
+    {productName:"Sudadera",designName:"buah",price:80,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"}
+  ];
+
+  newProducts: Product[] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getMore()
+  }
+
+  getMore() {
+    this.newProducts = this.newProducts.concat(this.products)
+    console.log(this.newProducts)
+  }
+
+}
