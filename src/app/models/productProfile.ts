165dc8e710c1e4b36b4aaf5648f6021f6fbcdec9
ibon0 <ibonlkz@gmail.com>
2022-10-17 09:47:12 +0200

    Basico perfil de producto, falta ponerlo bonito

--- src/app/models/productProfile.ts
+++ src/app/models/productProfile.ts
@@ -1,6 +1,7 @@
 export interface ProductProfile {
     productName: String,
     designName: String,
+    description:String,
     price: Number,
     mainImage: String,
     images: String[]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,9 +12,14 @@ export class ProductComponent implements OnInit{
 
 
   productProfile: ProductProfile = 
-    {productName:"Sudadera",designName:"buah",price:10,mainImage:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530",images:[]};
+    {productName:"Sudadera"
+    ,designName:"buah"
+    ,description:"Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100%Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100%"
+    ,price:10
+    ,mainImage:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"
+    ,images:[]};
 
-  viewProduct: ProductProfile = {productName:"null",designName:"null",price:0,mainImage:"null",images:["null"]}
+  viewProduct: ProductProfile = {productName:"null",designName:"null",description:"null",price:0,mainImage:"null",images:["null"]}
 
   constructor() { }
 
