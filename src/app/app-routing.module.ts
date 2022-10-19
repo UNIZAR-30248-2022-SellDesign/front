e7e5a02a3fb239f3e756458fedf2d2a555b31d6c
ibon0 <ibonlkz@gmail.com>
2022-10-19 09:23:45 +0200

    y

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+
 const routes: Routes = [
   {path:'product',component: ProductComponent}
 ];
