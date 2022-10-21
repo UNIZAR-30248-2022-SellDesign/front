3842fdc736dba5e9a2bee3969d6c5ab3b0b5a7fe
Gelpa99 <759406@unizar.es>
2022-10-21 12:57:28 +0200

    hide navBar on login/register pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'selldesign-front';
+  showBar = true;
+  constructor(public router: Router) {
+    this.router = router;
+  }
+
 }
- 
