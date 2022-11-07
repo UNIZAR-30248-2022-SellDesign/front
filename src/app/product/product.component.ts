7af9859047479d8d96f7a4df112ca1e9135ee315
ibon0 <ibonlkz@gmail.com>
2022-11-07 22:17:16 +0100

    Se consigue los productos con el mismo diseño

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,22 +12,6 @@ const backURI = "https://selldesign-backend.onrender.com/"
 export class ProductComponent implements OnInit{
 
 
-
-  productProfile: ProductProfile = 
-    {productName:"Sudadera"
-    ,designName:"molongo"
-    ,description:"Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100%Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100% Esta sudadera fue construida con poliester 100%"
-    ,price:10.99
-    ,favourite:true
-    ,favouriteImage: "https://cdn-icons-png.flaticon.com/512/1040/1040230.png"
-    ,mainImage:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"
-    ,designerName:"Alfred Marley"
-    ,imagesDesign:["https://images.asos-media.com/products/sudadera-gris-carbon-extragrande-con-capucha-y-estampado-en-la-espalda-de-asos-design/202691931-1-darkshadow?$n_240w$&wid=40&fit=constrain"
-,"https://images.asos-media.com/products/sudadera-gris-carbon-extragrande-con-capucha-y-estampado-en-la-espalda-de-asos-design/202691931-1-darkshadow?$n_240w$&wid=40&fit=constrain"
-,"https://images.asos-media.com/products/sudadera-gris-carbon-extragrande-con-capucha-y-estampado-en-la-espalda-de-asos-design/202691931-1-darkshadow?$n_240w$&wid=40&fit=constrain"
-,"https://images.asos-media.com/products/sudadera-gris-carbon-extragrande-con-capucha-y-estampado-en-la-espalda-de-asos-design/202691931-1-darkshadow?$n_240w$&wid=40&fit=constrain"
-]};
-
   viewProduct: ProductProfile = {
                                 productName:"null"
                                 ,designName:"null"
@@ -44,14 +28,8 @@ export class ProductComponent implements OnInit{
 
   ngOnInit(): void {
     this.getInfo()
-    //this.getInfoDesigner("Ey")
-    //this.getMore()
   }
 
-  getMore() {
-    this.viewProduct = this.productProfile
-    console.log(this.viewProduct)
-  }
   addToCart(){
     console.log("Añadido al carrito")
   }
@@ -62,8 +40,6 @@ export class ProductComponent implements OnInit{
     if(this.viewProduct.favourite) document.write("hola")
   }
   getInfo(){
-    //getInfo de nombreReal y descripcion   
-    //this.viewProduct = localStorage.getItem('userName')
     axios.get(backURI+"products/get/636166c94a3a5d54e4df05d6")
         .then(response => {
           // Obtenemos los datos
@@ -74,7 +50,7 @@ export class ProductComponent implements OnInit{
           this.viewProduct.price=response.data.price
           this.viewProduct.designName=response.data.design.name
           this.getInfoDesigner(response.data.design.designer)
-          this.getImagesOfDesigner(response.data.design.name)
+          this.getImagesOfDesign(response.data.design._id,response.data.description)
           
         })
         .catch(e => {
@@ -94,13 +70,19 @@ getInfoDesigner(id: String){
       console.log(e);
     })
 }
-getImagesOfDesigner(designName:String){
-  axios.get(backURI+"designs/"+designName)
+getImagesOfDesign(designName:String, description:String){
+  axios.get(backURI+"products/design/"+designName)
       .then(response => {
-        console.log(response)
         // Obtenemos los datos
         console.log(response.data);
-        //this.viewProduct.imagesDesign = response.data
+        const products = new Array(0)
+        
+        for(let i = 0; i < response.data.length && i < 4; i++) {
+          if(description !== response.data[i].description){
+            products.push(response.data[i].image);
+          }  
+        }
+        this.viewProduct.imagesDesign = products
       })
       .catch(e => {
         // Capturamos los errores
