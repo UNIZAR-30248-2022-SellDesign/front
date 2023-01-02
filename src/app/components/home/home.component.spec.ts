e523c0f1d80b2e9f540dded2dd4721a001e70c4a
rubenDTD <736650@unizar.es>
2023-01-02 21:27:30 +0100

    Register 96% + inicio home

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,23 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { BuscadorService } from '../../services/buscador.service';
+import { ArgumentService } from 'src/app/services/argument.service';
+import { of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import axios from 'axios';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
+  let buscadorService: BuscadorService;
+  let argumentService: ArgumentService;
   let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        BuscadorService,
+        ArgumentService
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    buscadorService = TestBed.inject(BuscadorService);
+    argumentService = TestBed.inject(ArgumentService);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit gets home products', () => {
+    // spyOn(axios, 'get').and.returnValue(Promise.resolve({ data: [{ product: 'abc' }, { product: 'def' }] }));
+    spyOn(axios, 'get').and.resolveTo({ 
+      response: {
+        data: [
+        { productName: 'ADSFASDF',
+          designName: "asdasdasd",
+          price: 13,
+          image: "sadasd" } 
+    ]}})
+    component.ngOnInit();
+    expect(axios.get).toHaveBeenCalledWith('https://selldesign-backend.onrender.com/products/home/page/0');
+    
+    expect(component.newProducts.length).toEqual(1);
+    expect(component.esNovedad).toEqual(true);
+    expect(component.noHayProductos).toEqual(false);
+    expect(component.hayMas).toEqual(true);
+  });
+  
+  
+  
+  
+  
+    
+  it('should make HTTP request and update products on getMore for search view', () => {
+    component.flagView = false;
+    component.contPageBusqueda = 0;
+    component.busqueda = 'test';
+    //spyOn(component, 'getSearch').and.returnValue(null);
+    component.getMore();
+    //expect(component.getSearch).toHaveBeenCalledWith('test', 1);
+    expect(component.newProducts.length).toEqual(0);
+    expect(component.contPageHome).toEqual(0);
+    expect(component.contPageBusqueda).toEqual(1);
+    expect(component.esNovedad).toEqual(false);
+    expect(component.esBusqueda).toEqual(false);
+    expect(component.hayMas).toEqual(false);
+    expect(component.noHayProductos).toEqual(false);
+    expect(component.precio).toEqual('Precio');
+    expect(component.tipo).toEqual('Prenda');
   });
 });
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   //   {productName:"Sudadera",designName:"buah",price:80,image:"https://static.pullandbear.net/2/photos/2022/I/0/2/p/8591/513/800/8591513800_1_1_3.jpg?t=1664869588530"}
   // ];
 
-  newProducts: any
+  newProducts: any = []
   contPageHome = 0
   contPageBusqueda = 0
   flagView = true
@@ -39,6 +39,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getIni()
     //this.argumentservice.currentargument.subscribe(message => this.message = message);
+    
     this._servicio.disparadorDeBusqueda.subscribe(data => {
 
       console.log('Result Tras disparador...', data.data.data);
